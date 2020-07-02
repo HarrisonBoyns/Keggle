957d65525eabb43e6a4232fb8fbc1ec434b25010
ClackerWhacker <boyns12345@gmail.com>
2020-07-02 10:02:56 +0100

    Update

--- Website/Website/settings.py
+++ Website/Website/settings.py
@@ -125,6 +125,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'Coming/static/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
