b4c1d7f589aecbbb7dfac03811473c860fee73ea
ClackerWhacker <boyns12345@gmail.com>
2020-07-16 13:31:13 +0100

    Update

--- Website/Website/settings.py
+++ Website/Website/settings.py
@@ -113,8 +113,8 @@ AUTH_PASSWORD_VALIDATORS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
-EMAIL_HOST_USER = 'learnhacktutoring@gmail.com'
-EMAIL_HOST_PASSWORD = 'LearnHack14!'
+EMAIL_HOST_USER = 'hitmebaby654321@gmail.com'
+EMAIL_HOST_PASSWORD = 'hitme1234!'
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 
